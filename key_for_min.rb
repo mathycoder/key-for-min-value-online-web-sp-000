58f328864242968404f6e46af7b989e956afeac3
Adam S <adam.sarli@gmail.com>
2019-03-14 13:33:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  smallest = new.Integer
+  print smallest
 end
+
+
+hash = {5 => "blah", 10 => "bleak"}
+key_for_min_value(hash)
